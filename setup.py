8de474256ed77db1d27b3748433ed2441a4fd6fb
Jeremy Delahanty <delahantyj@janelia.hhmi.org>
2026-07-01 20:42:31 -0400

    build: add pyproject with declared dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-# ~/gitrepos/palette/setup.py
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="fisheye",
-    version="0.1.0",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    entry_points={
-        "console_scripts": [
-            "palette-backfill-subject-body-mask-qc=fisheye.refinement.subject_body_mask_qc:main",
-            "palette-write-refined-subject-mask-edit=fisheye.utils.write_refined_subject_mask_edit:main",
-        ],
-    },
-    python_requires=">=3.11",
-)
+
+if __name__ == "__main__":
+    setup()
